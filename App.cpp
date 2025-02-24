35a7ed04ac208bec375656deb6159874190e19b4
Alejandro Jose Garcia Alfaro <alejandrojosegarcia75@gmail.com>
2025-02-24 08:31:53 -0600

    Made a Triangle

--- App.cpp
+++ App.cpp
@@ -1,7 +1,7 @@
 #include "App.h"
 
 App::App() :
-	wnd(800, 800, L"Dx11 Engine")
+	wnd(1920, 1080, L"Dx11 Engine")
 {}
 
 int App::Go() {
@@ -18,5 +18,7 @@ int App::Go() {
 void App::DoFrame() {
 	const float c = sin(timer.Peek()) / 2.0f + 0.5f;								//Sin wave color effect
 	wnd.Gfx().ClearBuffer(c, c, 1.0f);												
+	wnd.Gfx().DrawTestTriangle();
 	wnd.Gfx().EndFrame();															//We are currently presenting a frame
+
 }
