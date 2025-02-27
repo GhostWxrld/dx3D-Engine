970b98a799c00e8281f4f9effe053fb35f9bc413
Alejandro Jose Garcia Alfaro <alejandrojosegarcia75@gmail.com>
2025-02-27 16:06:40 -0600

    Added Constant buffers

--- App.cpp
+++ App.cpp
@@ -18,7 +18,7 @@ int App::Go() {
 void App::DoFrame() {
 	const float c = sin(timer.Peek()) / 2.0f + 0.5f;								//Sin wave color effect
 	wnd.Gfx().ClearBuffer(c, c, 1.0f);												
-	wnd.Gfx().DrawTestTriangle();
+	wnd.Gfx().DrawTestTriangle(timer.Peek());
 	wnd.Gfx().EndFrame();															//We are currently presenting a frame
 
 }
